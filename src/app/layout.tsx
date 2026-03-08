090fcbcca39e07400aba0eb86ed97c129b0c3709
gustavintavo8 <uo286277@uniovi.es>
2026-03-08 02:50:20 +0100

    Mas optimizaciones para movil

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,13 @@ export const metadata: Metadata = {
     "Organiza partidos de fútbol, equilibra equipos, lleva tus estadísticas y disfruta del deporte.",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
